35090a7eaf1a98373a2f2da1c6edbd786dcb7219
Jack Allen <Allenj25@nku.edu>
2020-04-26 13:47:10 -0400

    Unittests

--- wiki/web/tests/admin_test.py
+++ wiki/web/tests/admin_test.py
@@ -7,7 +7,5 @@ from wiki.web.routes import user_create
 
 class TestAdminPage(unittest.TestCase):
    
-   with app.app_context():
-        @mock.patch('render_template', return_value=1)
-        def test_admin(self, mocked_render):
-            self.assertEquals(user_create, 1)
+    def test_admin(self, mocked_render):
+        self.assertEquals(user_create, 1)
